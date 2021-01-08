481e0f15082fa0ac7a2c04a050fcce36ee592e57
NJKongelf <56114977+NJKongelf@users.noreply.github.com>
2021-01-08 16:21:42 +0100

    first commit

---
+++ src/main/java/se/iths/autofixjms/AutofixJmsApplication.java
@@ -0,0 +1,13 @@
+package se.iths.autofixjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutofixJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutofixJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/autofixjms/AutofixJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.autofixjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutofixJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
