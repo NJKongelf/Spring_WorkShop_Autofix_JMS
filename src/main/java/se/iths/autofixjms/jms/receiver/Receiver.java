9fd5e1d96ec30066bff86b798234440dcd4097c7
NJKongelf <56114977+NJKongelf@users.noreply.github.com>
2021-01-09 23:54:51 +0100

    Made Receiver to send emails

--- src/main/java/se/iths/autofixjms/jms/receiver/Receiver.java
+++ src/main/java/se/iths/autofixjms/jms/receiver/Receiver.java
@@ -7,18 +7,21 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import se.iths.autofixjms.jms.email.EmailServiceImpl;
+import se.iths.autofixjms.jms.model.MessageObject;
 
 import java.time.LocalDateTime;
 
 @Component
 public class Receiver {
 
-    Logger logger = LoggerFactory.getLogger(Receiver.class);
-    RabbitTemplate rabbitTemplate;
+    private Logger logger = LoggerFactory.getLogger(Receiver.class);
+    private RabbitTemplate rabbitTemplate;
+    private EmailServiceImpl emailService;
 
-    public Receiver(RabbitTemplate rabbitTemplate) {
-
+    public Receiver(RabbitTemplate rabbitTemplate, EmailServiceImpl emailService) {
         this.rabbitTemplate = rabbitTemplate;
+        this.emailService=emailService;
     }
 
 //    @Scheduled(fixedRate = 2000)
@@ -27,5 +30,9 @@ public class Receiver {
         logger.info("Message received: "+message);
 
     }
-
+    @RabbitListener(queues = "MailQueue")
+    public void receiveMessageMail(@Payload MessageObject messageObject) {
+        logger.info("Message Mail received: "+messageObject.getEmail());
+        emailService.sendSimpleMessage(messageObject.getEmail(),"Welcome to Autofix "+messageObject.getFirstname(), messageObject.getMessage());
+    }
 }
